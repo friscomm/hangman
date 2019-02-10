6ed533f00f208e513c7b926c8ad3b77f6b678569
Frisco Miranda <frisco.miranda@procore.com>
2019-02-10 07:48:42 -0800

    create wordpicker class to randomly choose game word

--- lib/word_picker.rb
+++ lib/word_picker.rb
@@ -1,11 +1,23 @@
 class WordPicker
+  attr_reader :word
 
-def get_word
+  def initialize
+    @word = get_word(words_array)
+  end
 
-  
+  def words_array
+    dictionary = File.open('dictionary.txt')
+    words = []
+    File.readlines(dictionary).each do |line|
+      if line.length > 5 && line.length < 12
+        words << line
+      end
+    end
+    words
+  end
 
-end
-
-
+  def get_word(words_array)
+    words_array.sample
+  end
 
 end
